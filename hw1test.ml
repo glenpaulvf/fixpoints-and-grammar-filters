99daa3061a7d3ff44d6ad99fcd5c0274688aeede
glenpaulvf <glen.florendo@gmail.com>
2017-10-01 14:36:49 -0700

    Define equal_sets tests

--- hw1test.ml
+++ hw1test.ml
@@ -4,3 +4,5 @@ let subset_test0 = subset [] [1;2;3]
 let subset_test1 = subset [3;1;3] [1;2;3]
 let subset_test2 = not (subset [1;3;7] [4;1;3])
 
+let equal_sets_test0 = equal_sets [1;3] [3;1;3]
+let equal_sets_test1 = not (equal_sets [1;3;4] [3;1;3])
