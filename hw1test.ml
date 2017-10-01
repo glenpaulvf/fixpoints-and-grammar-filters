475328ba6fabc2129028c0a06c19e9d9b18178af
glenpaulvf <glen.florendo@gmail.com>
2017-10-01 14:46:01 -0700

    Define set_union tests

--- hw1test.ml
+++ hw1test.ml
@@ -6,3 +6,7 @@ let subset_test2 = not (subset [1;3;7] [4;1;3])
 
 let equal_sets_test0 = equal_sets [1;3] [3;1;3]
 let equal_sets_test1 = not (equal_sets [1;3;4] [3;1;3])
+
+let set_union_test0 = equal_sets (set_union [] [1;2;3]) [1;2;3]
+let set_union_test1 = equal_sets (set_union [3;1;3] [1;2;3]) [1;2;3]
+let set_union_test2 = equal_sets (set_union [] []) []
