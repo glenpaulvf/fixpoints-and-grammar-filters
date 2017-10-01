e13810f6ab8beedbf33c4162322ad5bf9a5cd5dd
glenpaulvf <glen.florendo@gmail.com>
2017-10-01 14:45:08 -0700

    Implement set_union function with add_element helper function

--- hw1.ml
+++ hw1.ml
@@ -11,11 +11,18 @@ let rec (member : 'a -> 'a list -> bool) =
 	fun a b ->
 	match a with
 	|	[] -> true
-	| h::t ->
-		if (member h b) then subset t b 
-		else false
+	| h::t -> if (member h b) then subset t b else false
 
 let rec (equal_sets : 'a list -> 'a list -> bool) =
 	fun a b ->
 	subset a b && subset b a
 
+let rec (add_element : 'a -> 'a list -> 'a list) =
+	fun x s ->
+	if member x s then s else x::s
+
+let rec (set_union : 'a list -> 'a list -> 'a list) =
+	fun a b ->
+	match a with
+	| [] -> b
+	| h::t -> add_element h (set_union t b)
