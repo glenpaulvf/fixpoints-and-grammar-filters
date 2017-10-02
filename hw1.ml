8292e1e608849bed5f206553d4f28c58100bd8d2
Glen Paul Florendo <9373317+glenpaulvf@users.noreply.github.com>
2017-10-01 18:43:36 -0700

    Remove type generalizations

--- hw1.ml
+++ hw1.ml
@@ -1,28 +1,23 @@
 (* Homework 1. Fixpoints and grammar filters *)
 
-let rec (member : 'a -> 'a list -> bool) =
-	fun x s ->
+let rec member x s =
 	match s with
 	| [] -> false
 	| h::t when h = x -> true
 	| h::t -> member x t
 
- let rec (subset : 'a list -> 'a list -> bool) =
-	fun a b ->
+ let rec subset a b =
 	match a with
 	|	[] -> true
 	| h::t -> if member h b then subset t b else false
 
-let (equal_sets : 'a list -> 'a list -> bool) =
-	fun a b ->
+let equal_sets a b =
 	subset a b && subset b a
 
-let (add_element : 'a -> 'a list -> 'a list) =
-	fun x s ->
+let add_element x s =
 	if member x s then s else x::s
 
-let rec (set_union : 'a list -> 'a list -> 'a list) =
-	fun a b ->
+let rec set_union a b =
 	match a with
 	| [] -> b
 	| h::t -> add_element h (set_union t b)
