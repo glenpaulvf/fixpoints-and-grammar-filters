1fc49d456a8c7c9abf831be90d017e5147019195
Glen Paul Florendo <9373317+glenpaulvf@users.noreply.github.com>
2017-10-01 17:55:22 -0700

    Define set_intersection tests

--- hw1test.ml
+++ hw1test.ml
@@ -10,3 +10,11 @@ let equal_sets_test1 = not (equal_sets [1;3;4] [3;1;3])
 let set_union_test0 = equal_sets (set_union [] [1;2;3]) [1;2;3]
 let set_union_test1 = equal_sets (set_union [3;1;3] [1;2;3]) [1;2;3]
 let set_union_test2 = equal_sets (set_union [] []) []
+
+let set_intersection_test0 =
+  equal_sets (set_intersection [] [1;2;3]) []
+let set_intersection_test1 =
+  equal_sets (set_intersection [3;1;3] [1;2;3]) [1;3]
+let set_intersection_test2 =
+  equal_sets (set_intersection [1;2;3;4] [3;1;2;4]) [4;3;2;1]
+  
